16f89d3846894ca4176d082cdcc63ac9f3d83d21
aristidei <a.isingizwe@alustudent.com>
2024-05-17 12:46:37 +0200

    adding accross planes

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module is implementing logic of adding matrices
+"""
+
+
+def add_matrices(mat1, mat2):
+    """
+    function add matrices that adds two matrices
+    by adding two elements in the same position
+    """
+    if len(mat1) != len(mat2):
+        return None
+    result = []
+    for arr in range(len(mat1)):
+        if len(mat1[arr]) != len(mat2[arr]):
+            return None
+    innerArr = []
+    for num in range(len(mat1[arr])):
+        innerArr.append(mat1[arr][num] + mat2[arr][num])
+        result.append(innerArr)
+
+    return result
