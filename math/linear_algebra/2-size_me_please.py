eca448d571e8886bc0387c06113104d58bd4e865
aristidei <a.isingizwe@alustudent.com>
2024-05-17 10:18:57 +0200

    matrix shape

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,6 +5,7 @@
  this module will return the size of the array
 """
 
+
 def matrix_shape(matrix):
     """
     matrix: list of list
@@ -16,5 +17,6 @@ def matrix_shape(matrix):
         matrix = matrix[0]
     return shape
 
+
 if __name__ == "__main__":
-   matrix_shape_function = __import__('2-size_me_please').matrix_shape
+    matrix_shape_function = __import__('2-size_me_please').matrix_shape
