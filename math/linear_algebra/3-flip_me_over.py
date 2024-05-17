0158f5b80f69636f81052f0180c34a94f90fb299
aristidei <a.isingizwe@alustudent.com>
2024-05-17 10:29:57 +0200

    matrix shape

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,9 +5,16 @@ this module will return the transpose of a matrix
 this means that the rows will become columns and the columns will become rows
 """
 
+
 def matrix_transpose(matrix):
     """
     matrix: list of list
     return: list of list
     """
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    transpose = []
+    for i in range(len(matrix[0])):
+        row = []
+        for j in range(len(matrix)):
+            row.append(matrix[j][i])
+        transpose.append(row)
+    return transpose
